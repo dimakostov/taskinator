e5f68ca47d6f9980504339fd006457045c98a005
Dima Kostov <dimakostov@yahoo.com>
2021-05-22 19:49:45 -0400

    added form and form event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 var createTaskHandler = function() {
+    event.preventDefault();
+    
     var taskItemEl =document.createElement("li");
     taskItemEl.textContent = "This is a new task";
     taskItemEl.className = "task-item";
     tasksToDoEl.appendChild(taskItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
